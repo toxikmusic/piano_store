b5b11861ee35c45b2ba2af5bc7c7066ffaed0212
toxikmusic <153168348+toxikmusic@users.noreply.github.com>
2025-04-17 03:31:20 -0700

    Update index.js

--- client/src/dist/index.js
+++ client/src/dist/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../client/src/App';
-import '../client/src/index.css';
+import App from '../client/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null, React.createElement(App))
 );
